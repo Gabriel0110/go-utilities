598f8e853838b0ccb9cf92d8be22d9bb95d2d2d3
Gabriel Tomberlin <gabrieltomberlin14@gmail.com>
2021-12-30 10:05:25 -0600

    Add utilities.go

---
+++ utilities/utilities.go
@@ -0,0 +1,35 @@
+package utilities
+
+/*
+	Go utility functions for common tasks that aren't readily available like Python's
+*/
+
+// Check if a slice contains an int value
+func ContainsInt(slice []int, num int) bool {
+	for _, v := range slice {
+		if v == num {
+			return true
+		}
+	}
+	return false
+}
+
+// Check if a slice contains a float64 value
+func ContainsFloat64(slice []float64, num float64) bool {
+	for _, v := range slice {
+		if v == num {
+			return true
+		}
+	}
+	return false
+}
+
+// Check if a slice contains a string value
+func ContainsString(slice []string, str string) bool {
+	for _, v := range slice {
+		if v == str {
+			return true
+		}
+	}
+	return false
+}
