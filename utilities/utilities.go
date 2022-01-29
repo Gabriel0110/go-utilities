d2dae3621f6e0c360867ed1cc0067a5f4434abe1
Gabriel Tomberlin <gabrieltomberlin14@gmail.com>
2022-01-28 22:17:00 -0600

    Add binary search to utilities

--- utilities/utilities.go
+++ utilities/utilities.go
@@ -33,3 +33,19 @@ func ContainsString(slice []string, str string) bool {
 	}
 	return false
 }
+
+func binarySearch(arr []int, l int, r int, x int) int {
+	if r >= l {
+		mid := l + (r-1)/2
+
+		if arr[mid] == x {
+			return mid
+		} else if arr[mid] > x {
+			return binarySearch(arr, l, mid-1, x)
+		} else {
+			return binarySearch(arr, mid+1, r, x)
+		}
+	} else {
+		return -1
+	}
+}
